470760826a97348043d2d49520cfc33d104e4434
Gowri <gowri@ZipcodersMBP64.local>
2023-12-02 17:09:35 -0500

    tried few

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,4 +1,27 @@
 package io.zipcoder;
 
 public class Classroom {
+    private Student[] students;
+
+    public Classroom(Student[] students){
+        this.students = students;
+    }
+    public Classroom(int maxNumberOfStudents){
+        students = new Student[maxNumberOfStudents];
+    }
+    public Classroom(){
+        this(30);
+    }
+
+    public Student[] getStudents(){
+        return students;
+    }
+    public double getAverageExamScore() {
+        double sum = 0.0;
+        for (Student s : students) {
+            sum = sum + s.getAverageExamScore();
+        }
+        return sum / students.length;
+    }
+
 }
