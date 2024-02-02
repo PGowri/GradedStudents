c91f66eabad6b203edf8f0209e339f778f8f3780
PGowri <gowri.palaniswamy@gmail.com>
2024-02-01 23:28:35 -0500

    constructor test passing

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -9,7 +9,7 @@ import static java.util.ArrayList.*;
 public class Student {
     private String firstName;
     private String lastName;
-    private ArrayList<Double> examScores;
+    final ArrayList<Double> examScores;
 
 public Student() {
     this.examScores = new ArrayList<>();
--- src/test/java/io/zipcoder/StudentTest.java
+++ src/test/java/io/zipcoder/StudentTest.java
@@ -1,5 +1,23 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class StudentTest {
 
+    // constructor test
+    @Test
+    public void nullaryConstructorTest() {
+        //given
+        String firstName = null;
+        String lastName = null;
+        String examScores = "Exam Scores :\n";
+        //when
+        Student student1 = new Student();
+
+        //then
+        Assert.assertEquals(firstName, student1.getFirstName());
+        Assert.assertEquals(lastName, student1.getLastName());
+        Assert.assertEquals(examScores, student1.getExamScores());
+    }
 }
